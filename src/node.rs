ed0d31e1ec3cf13022f9ff1dff086a8a01fbe048
andy <andysalerno@gmail.com>
2020-08-31 00:31:10 -0700

    Add some more tests.

--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,4 @@
-use std::cell::{Ref, RefCell};
+use std::cell::{Ref, RefCell, RefMut};
 use std::rc::{Rc, Weak};
 
 #[derive(Default, Debug)]
@@ -23,7 +23,22 @@ impl<T> Node<T> {
         Self(Rc::new(internal))
     }
 
-    pub fn add_all_children(&mut self, children_data: impl IntoIterator<Item = T>) {
+    // pub fn add_all_children(&mut self, children_data: impl IntoIterator<Item = T>) {
+    //     let this_node = self.get_rc();
+    //     let mut children = children_data
+    //         .into_iter()
+    //         .map(|c| NodeInternal {
+    //             data: c,
+    //             parent: Rc::downgrade(this_node),
+    //             children: RefCell::new(Vec::new()),
+    //         })
+    //         .map(|i| Self(Rc::new(i)))
+    //         .collect();
+
+    //     this_node.children.borrow_mut().append(&mut children);
+    // }
+
+    pub fn add_all_children(&self, children_data: impl IntoIterator<Item = T>) {
         let this_node = self.get_rc();
         let mut children = children_data
             .into_iter()
@@ -60,6 +75,12 @@ impl<T> Node<T> {
         let rc = self.get_rc();
 
         rc.children.borrow()
+    }
+
+    pub fn parent(&self) -> Option<Self> {
+        let maybe_rc = self.get_rc().parent.upgrade();
+
+        maybe_rc.map(|rc| Self(rc))
     }
 
     fn get_rc(&self) -> &Rc<NodeInternal<T>> {
@@ -124,11 +145,60 @@ mod tests {
         // as expected. (thought I thought modern Rust would auto-limit the lifetime...)
         let data = {
             let children = root.children();
+            assert_eq!(1, children.len());
             *children[0].data()
         };
 
         root.add_child(49);
 
         assert_eq!(50, data);
+        assert_eq!(2, root.children().len());
+    }
+
+    #[test]
+    fn multiple_layers_of_children() {
+        let mut root = Node::new(42);
+
+        root.add_child(1);
+        root.add_child(2);
+
+        // Should not panic
+        let added_child = &root.children()[0];
+        added_child.add_all_children(vec![3, 4, 5]);
+
+        let added_child2 = &root.children()[1];
+        added_child2.add_all_children(vec![6, 7, 8, 9]);
+
+        assert_eq!(2, root.children().len());
+        assert_eq!(3, root.children()[0].children().len());
+        assert_eq!(4, root.children()[1].children().len());
+    }
+
+    #[test]
+    fn parent_when_node_has_parent() {
+        let mut root = Node::new(42);
+
+        root.add_child(1);
+
+        let child = &root.children()[0];
+
+        let parent = child
+            .parent()
+            .expect("In this test, the child *does* have a parent.");
+
+        assert_eq!(42, *parent.data());
+    }
+
+    #[test]
+    fn parent_when_node_is_root() {
+        let mut root = Node::new(42);
+
+        root.add_child(1);
+
+        let _child = &root.children()[0];
+
+        let parent = root.parent();
+
+        assert!(parent.is_none());
     }
 }
